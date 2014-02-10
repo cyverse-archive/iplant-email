61f684a15c1d9ba3a0d39f0de34ed70cfb5145a4
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-10 10:05:20 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
-(defproject iplant-email "1.2.1-SNAPSHOT"
+(defproject org.iplantc/iplant-email "1.2.2-SNAPSHOT"
   :description "iPlant Email Service"
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/iplant-email.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/iplant-email.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/iplant-email.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
                  [org.clojure/tools.logging "0.2.3"]
                  [cheshire "5.0.1"]
                  [javax.mail/mail "1.4"]
@@ -9,13 +18,17 @@
                  [compojure "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [log4j/log4j "1.2.16"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
   :iplant-rpm {:summary "iplant-email"
-               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin" "java-1.7.0-openjdk"]
+               :dependencies ["iplant-service-config >= 0.1.0-5"
+                              "iplant-clavin"
+                              "java-1.7.0-openjdk"]
                :config-files ["log4j.properties"]
                :config-path "conf"
                :resources ["*.st"]}
-  :repositories  {"iplantCollaborative"
-                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"}
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :deploy-repositories [["sonatype-nexus-staging"
+                         {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]]
   :aot [iplant-email.core]
   :main iplant-email.core)
